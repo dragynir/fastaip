f7edc024811195c6c7000cf728cd9f15de181bdc
dragynir <d.korostelevdn@gmail.com>
2024-01-13 15:46:44 +0700

    doc

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Backend")
 
 
-@app.get("/")
-def home():
-    return 'Hi new app 2!'
+fake_users = [
+    {'id': 1, 'role': 'admin', 'name': 'Alice'},
+    {'id': 2, 'role': 'traider', 'name': 'Bob'},
+]
+
+
+# Пример передачи параметра в пути запроса
+@app.get("/users/{user_id}")
+def get_user(user_id: int):  # тайпхинт важен, т к по нему user_id из строки кастуется к int
+    """Get user from database."""
+    return [user for user in fake_users if user['id'] == user_id]
