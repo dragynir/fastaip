739e034efa211da4e957243f696a67b59f39cac2
dragynir <d.korostelevdn@gmail.com>
2024-01-13 15:56:07 +0700

    routes

--- main.py
+++ main.py
@@ -9,8 +9,29 @@ fake_users = [
 ]
 
 
-# Пример передачи параметра в пути запроса
+# Пример передачи параметра пути user_id в пути запроса
+# Возвращаем список, который автоматисески сериализуется
+# Пример запроса
+# curl -X 'GET' \
+#   'http://127.0.0.1:8000/users/1' \
+#   -H 'accept: application/json'
 @app.get("/users/{user_id}")
 def get_user(user_id: int):  # тайпхинт важен, т к по нему user_id из строки кастуется к int
     """Get user from database."""
     return [user for user in fake_users if user['id'] == user_id]
+
+
+fake_trades = [
+    {'id': 1, 'user_id': 1, 'currency': 'btc'},
+    {'id': 2, 'user_id': 2, 'currency': 'btc'},
+]
+
+# Пример передачи параметров
+# Также тут проставлены дефолтные параметры
+# Пример запроса
+# curl -X 'GET' \
+#   'http://127.0.0.1:8000/trades/?limit=1&offset=1' \
+#   -H 'accept: application/json'
+@app.get("/trades/")
+def get_trades(limit: int = 0, offset: int = 1):
+    return fake_trades[offset:][:limit]
