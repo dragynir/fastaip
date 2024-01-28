817b080a6b9d2ac79a2a69000e832b94762de567
dragynir <d.korostelevdn@gmail.com>
2024-01-28 13:34:19 +0700

    add search with jinja

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 
+from operations.router import get_specific_operations
 
 router = APIRouter(
     prefix='/pages',
@@ -12,3 +13,11 @@ templates = Jinja2Templates(directory='templates')
 @router.get("/base")
 def get_base_page(request: Request):
     return templates.TemplateResponse('base.html', {'request': request})
+
+@router.get("/search/{operation_type}")
+def get_base_page(request: Request, operations=Depends(get_specific_operations)):
+    return templates.TemplateResponse('search.html', {'request': request, 'operations': operations['data']})
+
+@router.get("/search")
+def get_base_page(request: Request):
+    return templates.TemplateResponse('search.html', {'request': request})
